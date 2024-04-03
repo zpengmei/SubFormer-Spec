2de3f36831711848232a1289923692dbdf58a9a5
Zihan Pengmei <peng.mei2009@icloud.com>
2024-04-03 13:36:57 -0700

    update the packages

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from distutils.core import setup
 setup(
     name='SubFormer-Spec',
     version='0.01',
-    packages=[''],
+    packages=['torch', 'torch_geometric', 'torch_scatter',
+              'ogb', 'tqdm', 'networkx', 'rdkit', 'numpy', 'pandas',
+              'scipy','scikit-learn'],
     url='',
     license='',
     author='Zihan Pengmei',
-    author_email='',
-    description=''
+    author_email='zpengmei@uchicago.edu',
+    description='Implementation of SubFormer with the Spectral token for molecular property prediction.'
 )
